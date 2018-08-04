0105f6b3133897f4c1b11e094fa5c5730c4366b2
Lenovo <Lenovo@LAPTOP-QCG7OHU5>
2018-08-04 21:20:37 +0530

    Commited Welcome Project

---
+++ Welcome/src/com/demo/Accessable.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Accessable {
+
+	public static void main(String[] args) {
+		
+		Example e1 = new Example();
+		System.out.println(e1.name);
+		e1.compare();
+		
+
+	}
+
+}
---
+++ Welcome/src/com/demo/Example.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class Example {
+
+	public String name ="Narendra";
+	public  void compare()
+	{
+		System.out.println("Public isn't accessable");
+		
+	}
+	
+	
+}
